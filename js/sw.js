2d0d6ae22fbfd03f3770c0f3ee2e3c79809ae126
Lucie-Sluncikova <lucie.sluncikova@gmail.com>
2018-07-18 15:14:30 +0200

    add ignoreSearch parameter

--- js/sw.js
+++ js/sw.js
@@ -33,7 +33,7 @@ self.addEventListener('install', function(event) {
 //fetch the data
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request, {ignoreSearch: true}).then(function(response) {
       if(response) {
         console.log('it response');
         return response;
